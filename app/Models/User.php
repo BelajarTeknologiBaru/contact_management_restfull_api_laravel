caa28676baf86bc860b9a7e4ca08836da599bbfe
ahmaddzidnii <zdngaming312@gmail.com>
2024-04-15 22:05:18 +0700

    01:24:12 Register User API

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -11,12 +11,18 @@ class User extends Model
    protected $table = "users";
    protected $primaryKey = "id";
    protected $keyType ="int";
-   protected $timestamps = true ;
-   protected $incrementing = true ;
+   public $timestamps = true ;
+   public $incrementing = true ;
+
+   protected $fillable = [
+    'username',
+    'password',
+    'name'
+];
 
    public function contact():HasMany
    {
        return $this->hasMany(Contact::class, 'user_id', 'id');
    }
 
-}  
+}   

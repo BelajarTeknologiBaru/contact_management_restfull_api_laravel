07956902564a62415bc369c215766705546502c7
ahmaddzidnii <zdngaming312@gmail.com>
2024-04-16 15:14:00 +0700

    1:47:11 Update User API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -55,6 +56,24 @@ class UserController extends Controller
     public function getUser(Request $request):UserResource
     {
         $user = Auth::user();
+        return new UserResource($user);
+    }
+    public function updateUser(UserUpdateRequest $request):UserResource
+    {
+        $data = $request->validated();
+        $user_id = Auth::user()->id;
+
+        $user = User::find($user_id);
+
+        if(isset($data["name"])){
+            $user->name = $data["name"];
+        }
+
+        if(isset($data["password"])){
+            $user->password = Hash::make($data["password"]);
+        }
+
+        $user->save();
         return new UserResource($user);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,5 @@ Route::post("/users/login",[UserController::class,'login']);
 
 Route::middleware(ApiAuthMiddleware::class)->group(function(){
     Route::get("/users/current",[UserController::class,'getUser']);
+    Route::patch("/users/current",[UserController::class,'updateUser']);
 });

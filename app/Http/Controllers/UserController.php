6200183734e28a2cc88d5e0132f0477a2cbe800a
ahmaddzidnii <zdngaming312@gmail.com>
2024-04-16 14:15:28 +0700

    1:24:13 Login User API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Str;
 class UserController extends Controller
 {
     public function register(UserRegisterRequest $request)  {
@@ -29,5 +30,24 @@ class UserController extends Controller
 
         return (new UserResource($user))->response()->setStatusCode(201);
         
+    }
+    public function login(UserLoginRequest $request):UserResource{
+        $data = $request->validated();
+
+        $user = User::where("username", $data["username"])->first();
+
+        if(!$user || !Hash::check($data["password"], $user->password)) {
+            throw new HttpResponseException(response([
+                "errors"=>[
+                    "message"=>["Invalid username or password"
+                    ]
+                ]
+            ], 401));
+        }
+
+        $user->token = Str::uuid()->toString();
+        $user->save();
+
+        return new UserResource($user);
     }
 }
